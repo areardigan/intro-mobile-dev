d20338ee8d2a0a8a42713595542adc13a4f1464b
andrewreardigan <drewreardigan@gmail.com>
2022-08-29 00:48:33 -0400

    Text changing color onClick.

--- intro-mobile-dev/ViewController.swift
+++ intro-mobile-dev/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.red
+    }
+    
 }
 
